6736d1fefb9ee386811f924cd68fd753955c46d5
litinoveweedle <15144712+litinoveweedle@users.noreply.github.com>
2025-03-12 22:19:55 +0100

    Initial commit added SQLite support added log messages added help added command line named parameters added support for entities names added support to fix all recorder entitites fixed sum calculation

--- fix.py
+++ fix.py
@@ -1,132 +1,335 @@
-# Module For Connecting To MySQL database
 import sys
-import MySQLdb
-import MySQLdb.cursors
+import re
+import getopt
+from pathlib import Path
+
 
 # Function for connecting to MySQL database
-def connect():
+def connect_db():
     # Trying to connect
     try:
-        db_connection = MySQLdb.connect("localhost", "root", sys.argv[1], sys.argv[2])
+        if db == "sqlite":
+            db_connection = sqlite3.connect(database)
+            db_connection.row_factory = sqlite3.Row
+        elif db == "mysql":
+            db_connection = MySQLdb.connect(
+                database[3], database[1], database[2], database[4]
+            )
     # If connection is not successful
-    except MySQLdb.Error:
-        print("Can't connect to database")
-        return 0
+    except Exception as e:
+        print(f"An error occurred connecting to DB: {e}")
+        exit(1)
     # If Connection Is Successful
     print("Connected")
-
     return db_connection
 
 
-def fix_record(dbconn3, id, new_sum):
-    cursor = dbconn3.cursor()
-    cursor.execute("UPDATE statistics SET sum = %s WHERE id = %s", (new_sum, id))
+def fix_record(db_conn3, id, new_sum):
+    cursor = db_conn3.cursor()
+    qry = "UPDATE statistics SET sum = {} WHERE id = {}".format(new_sum, id)
+    # print(qry)
+    cursor.execute(qry)
 
-def fix_record_short(dbconn3, id, new_sum):
-    cursor = dbconn3.cursor()
-    cursor.execute("UPDATE statistics_short_term SET sum = %s WHERE id = %s", (new_sum, id))
+
+def fix_record_short(db_conn3, id, new_sum):
+    cursor = db_conn3.cursor()
+    qry = "UPDATE statistics_short_term SET sum = {} WHERE id = {}".format(new_sum, id)
+    # print(qry)
+    cursor.execute(qry)
+
+
+def help(error):
+    if error:
+        print("\nERROR: " + error)
+    print("\nWARNING!!!")
+    print("Read and follow these steps:")
+    print("1. Stop the HomeAssistant.")
+    print(
+        "2. Backup your HomeAssistant database, really, really, really, pretty please."
+    )
+    print(
+        "3. Correct any erroneous recorder data (typically 'spikes' anomalies) in the HomeAssistant"
+    )
+    print(
+        "   database tables 'statistics_short_term' and/or 'statistics' in the column 'state'."
+    )
+    print("4. Execute this tool, see options bellow.")
+    print("5. Restart the HomeAssistant.")
+    print("\nUSAGE:")
+    print(
+        "# python3 fix.py -h | -d|--database DB (-e|--entities_names ENT)|(-i|--entities_ids ENT_ID)(|-a|--all)"
+    )
+    print("-h\t--help\t\t\tprint this help")
+    print(
+        "-d\t--database\t\tpath to SQLite db file or MYSQL connect string: user:password@ip:database"
+    )
+    print(
+        "-e\t--entities_names \tcomma separated list of the recorder entities names to correct, typically sensors entities"
+    )
+    print(
+        "-i\t--entities_ids \t\tcomma separated list of the recorder entities IDs to correct (id from the statistic_meta table)"
+    )
+    print("-a\t--all\t\t\tcorrect records for all recorder entities\n")
+    if error:
+        exit(1)
+    exit(0)
+
+
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+
+# Options
+options = "hd:e:i:a"
+
+# Long options
+long_options = ["help", "database=", "entities_names=", "entities_ids=", "all"]
+
+entities_all = None
+entities_ids = None
+entities_names = None
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-h", "--help"):
+            help(None)
+        elif currentArgument in ("-d", "--database"):
+            database = currentValue
+        elif currentArgument in ("-e", "--entities_names"):
+            entities_names = currentValue
+        elif currentArgument in ("-i", "--entities_ids"):
+            entities_ids = currentValue
+        elif currentArgument in ("-a", "--all"):
+            entities_all = 1
+
+except getopt.error as err:
+    # output error, and return with an error code
+    help("Invalid input: " + str(err))
+
+if database is None:
+    help("Database files must be specified!")
+elif match := re.match("([^\:]+)\:([^\@])+\@([^\:]+)\:(.+)", database):
+    import MySQLdb
+    import MySQLdb.cursors
+
+    db = "mysql"
+    database = [match.group(1), match.group(2), match.group(3), match.group(4)]
+elif Path(database).is_file():
+    import sqlite3
+
+    db = "sqlite"
+else:
+    help("Invalid database parameter")
+
+if entities_names:
+    entities_names = list(map(str.strip, entities_names.split(",")))
+    if not len(entities_names) or not (
+        all([re.match("\w+\.\w+", val) for val in entities_names])
+    ):
+        help("Entities shall be comma separated list of entities names")
+elif entities_ids:
+    entities_ids = list(map(str.strip, entities_ids.split(",")))
+    if not len(entities_ids) or not (
+        all([isinstance(val, int) for val in entities_ids])
+    ):
+        help("Entities shall be comma separated list of entities IDs")
+elif entities_all:
+    print("Executing for all recorder entities")
+else:
+    help("No entities to repair were specified")
 
 
 # Function Call For Connecting To Our Database
-dbconn1 = connect()
-dbconn2 = connect()
-dbconn3 = connect()
+db_conn1 = connect_db()
+db_conn2 = connect_db()
+db_conn3 = connect_db()
 
 # Making Cursor Object For Query Execution
-cursor = dbconn1.cursor(MySQLdb.cursors.DictCursor)
+if db == "sqlite":
+    cursor = db_conn1.cursor()
+elif db == "mysql":
+    cursor = db_conn1.cursor(MySQLdb.cursors.DictCursor)
 
 # Executing Query
-cursor.execute("SELECT * FROM statistics_meta WHERE has_sum = 1 AND id = {};".format(sys.argv[3]))
+if entities_names:
+    cursor.execute(
+        "SELECT * FROM statistics_meta WHERE has_sum = 1 AND statistic_id IN ({})".format(
+            result=", ".join(entities_names)
+        )
+    )
+if entities_ids:
+    cursor.execute(
+        "SELECT * FROM statistics_meta WHERE has_sum = 1 AND id IN ({});".format(
+            result=", ".join(entities_ids)
+        )
+    )
+else:
+    cursor.execute("SELECT * FROM statistics_meta WHERE has_sum = 1")
 
 # Above Query Gives Us The Current Date
 # Fetching Data
 records = cursor.fetchall()
 
 for row in records:
-    last_sum = 0.0
-    prev_sum = 0.0
-    prev_corrected_sum = 0.0
-    prev_state = 0.0
-    correction = 0.0
-    last_start = 0
-    print("To Fix: ", row['statistic_id'])
-    cursor2 = dbconn2.cursor(MySQLdb.cursors.DictCursor)
-    qry = "SELECT * FROM statistics WHERE metadata_id = {} ORDER BY start".format(row['id'])
+    row = dict(row)
+    prev_sum = None
+    prev_state = None
+    last = {"id": None, "start": None, "state": None, "sum": None}
+
+    print("- Checking Entity: {} Id: {}".format(row["statistic_id"], row["id"]))
+    cursor2 = db_conn2.cursor()
+    qry = "SELECT * FROM statistics WHERE metadata_id = {} ORDER BY start_ts".format(
+        row["id"]
+    )
     cursor2.execute(qry)
     stats = cursor2.fetchall()
     for stat in stats:
-        # Previous sum was bigger, so there was a reset
-        if stat['sum'] < prev_sum:
-            print("Prev SUM {} and current sum {}".format(prev_sum, stat['sum']))
-            # Last sum should contain sum before the reset + previous sum
-            # As the new sums should be calculated against the previous sum
-            last_sum = last_sum + prev_sum + correction
-            # As the current sum doesn't include all data, we need to use prev state to calculate the sum
-            sum = (stat['state'] - prev_state) + last_sum
-            # As for the next calculations we use the reset sum, we need to take the correction with us
-            correction = (stat['state'] - prev_state) - stat['sum']
-            print("Stat {} was reset at {}".format(row['id'], stat['state']))
-            print("Sum was {} and should be {} ({} {} {})".format(stat['sum'], sum, last_sum, prev_sum, correction))
-            prev_sum = stat['sum']
-            prev_corrected_sum = sum
-            fix_record(dbconn3, stat['id'], sum)
-        elif stat['sum'] < prev_corrected_sum:
-            sum = stat['sum'] + last_sum + correction
-            print("Sum2 was {} and should be {} ({} {} {})".format(stat['sum'], sum, last_sum, prev_sum, correction))
-            fix_record(dbconn3, stat['id'], sum)
-            prev_sum = stat['sum']
-            prev_corrected_sum = sum
-            prev_state = stat['state']
-        else:
-            prev_sum = stat['sum']
-            prev_state = stat['state']
-        last_start = stat['start']
+        stat = dict(stat)
+        if stat["state"] is None:
+            # skip rows with NULL state values
+            continue
+        elif prev_state is None:
+            # set initial values
+            prev_state = stat["state"]
+            prev_sum = stat["sum"]
+        elif stat["state"] > prev_state:
+            prev_sum = prev_sum + (stat["state"] - prev_state)
+        elif stat["state"] < prev_state:
+            prev_sum = prev_sum + stat["state"]
 
-    dbconn3.commit()
+        if prev_sum != stat["sum"]:
+            print(
+                "-  Statistics Record: {} has invalid sum: {}, shall be: {}".format(
+                    stat["id"], stat["sum"], prev_sum
+                )
+            )
+            fix_record(db_conn3, stat["id"], prev_sum)
 
-    cursor2 = dbconn2.cursor(MySQLdb.cursors.DictCursor)
+        prev_state = stat["state"]
+        last["id"] = stat["id"]
+        last["sum"] = prev_sum
+        last["state"] = stat["state"]
+        last["start"] = stat["start_ts"]
+
+    db_conn3.commit()
+
+    if last["start"] is None or last["sum"] is None:
+        print("ERROR: no valid Statistic Record was found")
+        continue
+
+    if db == "sqlite":
+        cursor = db_conn2.cursor()
+    elif db == "mysql":
+        cursor = db_conn2.cursor(MySQLdb.cursors.DictCursor)
+
+    if last["start"] is None or last["sum"] is None:
+        print("ERROR: no valid Statistic Record was found")
+        continue
 
     # Get last known value from short_temp
-    qry = "SELECT * FROM statistics_short_term WHERE metadata_id = {} ORDER BY start".format(row['id'])
+    qry = "SELECT * FROM statistics_short_term WHERE metadata_id = {} ORDER BY start_ts".format(
+        row["id"]
+    )
     cursor2.execute(qry)
     stats = cursor2.fetchall()
-    id = 0
-    prev_sum_invalid = 0.0
-    # Save the last SUM from statistics as we will use this value as a base
-    prev_sum_base = prev_corrected_sum
+
+    prev_sum = None
+    prev_state = None
     for stat in stats:
-        if stat['start'] == last_start:
-            print("Found base entry with id {}".format(stat['id']))
-            id = stat['id']
-            prev_sum_invalid = stat['sum']
-            fix_record_short(dbconn3, id, prev_sum_base)
-        elif id != 0:
-            prev_sum = prev_sum + (stat['sum'] - prev_sum_invalid)
-            prev_sum_invalid = stat['sum']
-            fix_record_short(dbconn3, stat['id'], prev_sum)
+        stat = dict(stat)
+        if stat["state"] is None:
+            # skip rows with NULL state values
+            continue
+        elif stat["start_ts"] == last["start"]:
+            # first row to start checking, initialize values
+            prev_sum = last["sum"]
+            prev_state = stat["state"]
+            if stat["state"] != last["state"]:
+                print(
+                    "-  Short Statistic record: {} has different state value: {} than Statistic record: {} state value: {}".format(
+                        stat["id"], stat["state"], last["id"], last["state"]
+                    )
+                )
+                # exit(1)
+        elif prev_state is None:
+            # skip rows before last_start
+            continue
+        if stat["state"] > prev_state:
+            # calculate expected correct sum value
+            prev_sum = prev_sum + (stat["state"] - prev_state)
+        elif stat["state"] < prev_state:
+            # calculate expected correct sum value
+            prev_sum = prev_sum + stat["state"]
 
-    id = 0
-    prev_sum_invalid = 0.0
-    # Our starting point is again the good sum value from statistics table
-    prev_sum = prev_sum_base
-    # We loop backwards to fix older data
+        if prev_sum != stat["sum"]:
+            # correct sum if different than expected
+            print(
+                "-  Short Statistics Record: {} has invalid sum: {}, shall be: {}".format(
+                    stat["id"], stat["sum"], prev_sum
+                )
+            )
+            fix_record_short(db_conn3, stat["id"], prev_sum)
+
+        prev_state = stat["state"]
+    if prev_state is None:
+        print(
+            "ERROR: no Short Statistic Record was found for start_ts: {}".format(
+                last["start"]
+            )
+        )
+
+    next_sum = None
+    next_state = None
     for stat in reversed(stats):
-        if stat['start'] == last_start:
-            print("Found base entry with id {}".format(stat['id']))
-            id = stat['id']
-            prev_sum_invalid = stat['sum']
-            prev_state = stat['state']
-            fix_record_short(dbconn3, id, prev_sum_base)
-        elif id != 0:
-            # There was another reset here, as current sum
-            # Fallback to state calculation then
-            if (prev_sum_invalid - stat['sum']) < 0:
-                prev_sum = prev_sum - (prev_state - stat['state'])
-            else:
-                prev_sum = prev_sum - (prev_sum_invalid - stat['sum'])
-            prev_sum_invalid = stat['sum']
-            prev_state = stat['state']
-            fix_record_short(dbconn3, stat['id'], prev_sum)
+        stat = dict(stat)
+        if stat["state"] is None:
+            # skip rows with NULL state values
+            continue
+        elif stat["start_ts"] == last["start"]:
+            # first row to start checking, initialize values
+            next_sum = last["sum"]
+            next_state = stat["state"]
+            if stat["state"] != last["state"]:
+                print(
+                    "-  Short Statistic record: {} has different state value: {} than Statistic record: {} state value: {}".format(
+                        stat["id"], stat["state"], last["id"], last["state"]
+                    )
+                )
+                # exit(1)
+        elif next_state is None:
+            # skip rows after last_start
+            continue
+        elif stat["state"] < next_state:
+            # calculate expected correct sum value
+            next_sum = next_sum - (next_state - stat["state"])
+        elif stat["state"] > next_state:
+            # calculate expected correct sum value
+            next_sum = next_sum - next_state
 
-dbconn3.commit()
+        if next_sum != stat["sum"]:
+            # correct sum if different than expected
+            print(
+                "-  Short Statistics record: {} has invalid sum: {}, shall be: {}".format(
+                    stat["id"], stat["sum"], next_sum
+                )
+            )
+            fix_record_short(db_conn3, stat["id"], next_sum)
 
+        next_state = stat["state"]
+    if next_state is None:
+        print(
+            "ERROR: no Short Statistic Record was found for start_ts: {}".format(
+                last["start"]
+            )
+        )
+
+    db_conn3.commit()
+
+db_conn1.close()
+db_conn2.close()
+db_conn3.close()
+print("Disconnected")
